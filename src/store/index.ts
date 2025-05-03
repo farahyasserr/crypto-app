d9d0d594e18174fcc22934743d8712700c35cf64
farahyasserr <farah.dawoudd@gmail.com>
2025-05-03 18:54:24 +0300

    feat: Store created

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { apiSlice } from "@/src/services/apiSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+export const store = configureStore({
+  reducer: {
+    // Add the generated reducer as a specific top-level slice
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling, etc.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
+
+// Optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+setupListeners(store.dispatch);
+
+// Infer the RootState and AppDispatch types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
