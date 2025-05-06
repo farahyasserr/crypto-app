75bcd83e38ee8bb1dce38b56b4da3af5cf0c749c
farahyasserr <farah.dawoudd@gmail.com>
2025-05-07 00:44:32 +0300

    feat: store functions created for auth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,21 +1,28 @@
 import * as SecureStore from "expo-secure-store";
 import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../store/selectors";
+import { selectIsUserAuthenticated } from "../store/selectors/authSelector";
+import { setAuthState } from "../store/slices/authSlice";
+import { RootState } from "../store/store";
 
 const useAuth = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const dispatch = useAppDispatch();
   const [isLoading, setIsLoading] = useState(true);
+  const isUserAuthenticated = useAppSelector((state: RootState) =>
+    selectIsUserAuthenticated(state)
+  );
 
   useEffect(() => {
     const checkLoginStatus = async () => {
-      const loggedIn = await SecureStore.getItemAsync("loggedIn");
-      setIsAuthenticated(loggedIn === "true");
+      const loggedIn = await SecureStore.getItemAsync("isAuthenticated");
+      dispatch(setAuthState(loggedIn === "true"));
       setIsLoading(false);
     };
 
     checkLoginStatus();
-  }, []);
+  }, [dispatch]);
 
-  return { isAuthenticated, isLoading };
+  return { isUserAuthenticated, isLoading };
 };
 
 export default useAuth;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { apiSlice } from "@/src/services/apiSlice";
+import authReducer from "@/src/store/slices/authSlice";
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     // Add the generated reducer as a specific top-level slice
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
