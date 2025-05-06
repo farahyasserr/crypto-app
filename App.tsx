e42752c0cd44b9ee19dd555fd54d0f4d34f1d153
farahyasserr <farah.dawoudd@gmail.com>
2025-05-06 23:28:18 +0300

    feat: Status bar added

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { I18nextProvider } from "react-i18next";
+import { StatusBar } from "react-native";
 import { Provider } from "react-redux";
 import useAuth from "./src/hooks/useAuth";
 import useLoadFonts from "./src/hooks/useLoadFonts";
@@ -22,6 +23,7 @@ export default function App() {
     <Provider store={store}>
       <I18nextProvider i18n={i18n}>
         <NavigationContainer>
+          <StatusBar animated barStyle="light-content" />
           <RootNavigator isAuthenticated={isAuthenticated} />
         </NavigationContainer>
       </I18nextProvider>
