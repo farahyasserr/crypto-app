7b3451487df575deb7171b164b00af07a4734506
farahyasserr <farah.dawoudd@gmail.com>
2025-05-07 00:45:59 +0300

    refactor: Custom hook refactoring

--- App.tsx
+++ App.tsx
@@ -3,28 +3,24 @@ import React from "react";
 import { I18nextProvider } from "react-i18next";
 import { StatusBar } from "react-native";
 import { Provider } from "react-redux";
-import useAuth from "./src/hooks/useAuth";
 import useLoadFonts from "./src/hooks/useLoadFonts";
 import i18n from "./src/locales/i18n";
 import { RootNavigator } from "./src/navigation/RootNavigator";
-import { store } from "./src/store";
+import { store } from "./src/store/store";
 
 export default function App() {
   const fontsLoaded = useLoadFonts();
-  const { isAuthenticated, isLoading } = useAuth();
 
   if (!fontsLoaded) {
     return null;
   }
-
-  if (isLoading) return null; // TODO: Can be enhanced to show a splash/loading screen
 
   return (
     <Provider store={store}>
       <I18nextProvider i18n={i18n}>
         <NavigationContainer>
           <StatusBar animated barStyle="light-content" />
-          <RootNavigator isAuthenticated={isAuthenticated} />
+          <RootNavigator />
         </NavigationContainer>
       </I18nextProvider>
     </Provider>
